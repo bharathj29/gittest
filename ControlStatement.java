569824cad45ce6229e9ad687e4bf6af79aca5215
bharathj29 <157878588+bharathj29@users.noreply.github.com>
2024-01-27 14:14:20 +0530

    Add files via upload

---
+++ ControlStatement.java
@@ -0,0 +1,17 @@
+package com.selinium;
+
+public class ControlStatement {
+   
+	public static void main(String[] args) {
+		int a=11;
+		   if(a==10) {
+			   
+			   
+			   System.out.println("a value is 10");
+		   }
+		   else {
+			   System.out.println("a value is not 10");
+		   }
+	}
+
+}
